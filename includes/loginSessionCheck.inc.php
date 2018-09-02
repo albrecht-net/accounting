06244875ef994d429df8ed42cbb142ec39f473f7
albrecht-net <jonasalbrecht9@gmail.com>
2018-09-02 14:05:10 +0200

    Login Check gibt nun Bool als Antwort zurück

--- includes/loginSessionCheck.inc.php
+++ includes/loginSessionCheck.inc.php
@@ -9,19 +9,20 @@ $dataSession = array(
 
 // Überprüft ob User-ID oder Benutzername in der Session leer sind (leer entspricht: kein Benutzer angemeldet)
 if (empty($dataSession['uid']) || empty($dataSession['username'])) {
-    // Mögliche Session löschen
-    session_destroy();
-    header("Location: login.php");
-	exit();
+    return FALSE;
 } elseif (isset($dataSession['uid']) && isset($dataSession['username'])) {
 	// SQL-Query bereitstellen
     $sqlquery = "SELECT * FROM `users` WHERE `uid` = '" . $dataSession['uid'] . "' AND `username` = '" . $dataSession['username'] . "'";
     
-	// Prüft ob Session mit Datenbank übereinstimmt (Wenn 1 Resultat: Benutzer vorhanden und korrekt abgeglichen, Session wird nicht zurückgesetzt)
+	// Prüft ob Sessionangaben mit Datenbank übereinstimmt
 	if (mysqli_num_rows(mysqli_query($config['link'], $sqlquery)) != 1) {
-		echo date('H:i:s') . ' Benutzer abfragen fehlgeschlagen';
-		unsetSession();
-		exit();
+		unset($_SESSION['uid']);
+		unset($_SESSION['username']);
+		return FALSE;
+	} else {
+		return TRUE;
 	}
+} else {
+	return FALSE;
 }
 ?>

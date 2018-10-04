b7a90837fc34bc6dd686b4c660f35df4a988af6a
albrecht-net <jonasalbrecht9@gmail.com>
2018-10-04 20:44:42 +0200

    Navigation zu buchung.php hinzugefügt

--- buchung.php
+++ buchung.php
@@ -19,6 +19,45 @@
     <title>Buchung erfassen</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Buchhaltung</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Neue Buchung <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Weitere erfassen
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item disabled" href="#">Konto</a>
+                        <a class="dropdown-item disabled" href="#">Empfänger</a>
+                        <a class="dropdown-item disabled" href="#">Klassifikation</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item disabled" href="#">Mein Profil</a>
+                        <a class="dropdown-item disabled" href="#">Einstellungen</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout.php">Abmelden</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <form>
         <div class="form-group">
             <label for="datum">Buchunsdatum</label>

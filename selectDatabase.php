18b75ee6c84d4be2c562cc85ebafd08e1d80e19b
albrecht-net <jonasalbrecht9@gmail.com>
2018-10-04 20:46:38 +0200

    Zoomen auf Mobilen geräten verhindern

--- selectDatabase.php
+++ selectDatabase.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit']) && !empty($_POST['dbID'])) {
 <html lang="de">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
--- setup.php
+++ setup.php
@@ -19,7 +19,7 @@ $dataSetup = array(
 <html lang="de">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">

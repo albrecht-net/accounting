10d8d3fe29db688cfe08dbe40e2b38ed6f3962ca
albrecht-net <jonasalbrecht9@gmail.com>
2018-10-29 21:12:09 +0100

    Styling Database Seite Einstellungen

--- settings/database.php
+++ settings/database.php
@@ -70,69 +70,76 @@ if (isset($_POST['submitAddDb'])) {
         </div>
     </nav>
 
-    <!-- Settings Navigation -->
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link" href="account.php">Account</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active" href="database.php">Datenbank</a>
-        </li>
-    </ul>
-
-    <h3>Datenbank hinzufügen</h3>
-
-    <!-- Statusmeldungen -->
-    <?php if ($msg['successAddDb']): ?>
-    <div class="alert alert-primary" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>        
-        <h4>Neue Datenbank erfolgreich gespeichert</h4>
-        <?php if (mysqli_num_rows($result) >= 1): ?>
-        <p>Es wurden folgende Tabellen in der Datenbank gefunden:<p>
-        <ul class="list-unstyled">
-            <?php while ($row = mysqli_fetch_row($result)): ?>
-            <li><?php echo $row[0]; ?></li>
-            <?php endwhile; ?>
+    <div class="container">
+        <!-- Settings Navigation -->
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link" href="account.php">Account</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="database.php">Datenbank</a>
+            </li>
         </ul>
-        <?php else: ?>
-        <p><i>In dieser Datenbank wurden keine Tabellen erkannt</i></p>
-        <?php endif; ?>
-    </div>
-    <?php elseif ($msg['tempLinkError']): ?>
-    <div class="alert alert-danger" role="alert">
-        <h4 class="alert-heading">Verbindung fehlgeschlagen!</h4>
-        <p>Es wurde vergeblich versucht eine temporäre Verbindung zur angegebenen Datenbank aufzubauen. Bitte überprüfen Sie die Angaben.</p>
-        <hr>
-        <p class="mb-0">Folgender Fehler wurde von MySQL ausgegeben: <i><?php echo mysqli_connect_error($tempLink); ?></i></p>
-    </div>
-    <?php endif; ?>
 
-    <form method="POST" action="database.php">
-        <div class="form-group">
-            <label for="dbHost">Server IP oder Hostname</label>
-            <input type="text" class="form-control" name="dbHost" id="dbHost" value="<?php echo $_GET['dbHost']; ?>" required>
+        <h3 class="py-3">Datenbank hinzufügen</h3>
+
+        <div class="row">
+            <div class="col-12">
+                <!-- Statusmeldungen -->
+                <?php if ($msg['successAddDb']): ?>
+                <div class="alert alert-primary" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>        
+                    <h4>Neue Datenbank erfolgreich gespeichert</h4>
+                    <?php if (mysqli_num_rows($result) >= 1): ?>
+                    <p>Es wurden folgende Tabellen in der Datenbank gefunden:<p>
+                    <ul class="list-unstyled">
+                        <?php while ($row = mysqli_fetch_row($result)): ?>
+                        <li><?php echo $row[0]; ?></li>
+                        <?php endwhile; ?>
+                    </ul>
+                    <?php else: ?>
+                    <p><i>In dieser Datenbank wurden keine Tabellen erkannt</i></p>
+                    <?php endif; ?>
+                </div>
+                <?php elseif ($msg['tempLinkError']): ?>
+                <div class="alert alert-danger" role="alert">
+                    <h4 class="alert-heading">Verbindung fehlgeschlagen!</h4>
+                    <p>Es wurde vergeblich versucht eine temporäre Verbindung zur angegebenen Datenbank aufzubauen. Bitte überprüfen Sie die Angaben.</p>
+                    <hr>
+                    <p class="mb-0">Folgender Fehler wurde von MySQL ausgegeben: <i><?php echo mysqli_connect_error($tempLink); ?></i></p>
+                </div>
+                <?php endif; ?>
+
+                <form method="POST" action="database.php">
+                    <div class="form-group">
+                        <label for="dbHost">Server IP oder Hostname</label>
+                        <input type="text" class="form-control" name="dbHost" id="dbHost" value="<?php echo $_GET['dbHost']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="dbPort">Port</label>
+                        <input type="number" class="form-control" name="dbPort" id="dbPort" min="0" value="<?php echo $_GET['dbPort']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="dbUsername">Benutzername</label>
+                        <input type="text" class="form-control" name="dbUsername" id="dbUsername" value="<?php echo $_GET['dbUsername']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="dbPassword">Passwort</label>
+                        <input type="password" class="form-control" name="dbPassword" id="dbPassword" required>
+                        <small id="dbPassword" class="form-text text-muted">Hinweis: Das Passwort wird in Klartext in der Datenbank gespeichert! Verwenden Sie einen Datenbank-Benutzer mit eingeschränkten Rechten.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="dbName">Datenbankname</label>
+                        <input type="text" class="form-control" name="dbName" id="dbName" value="<?php echo $_GET['dbName']; ?>" required>
+                    </div>
+                    <hr class="mb-4">
+                    <button type="submit" class="btn btn-primary" name="submitAddDb">Hinzufügen</button>
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="dbPort">Port</label>
-            <input type="number" class="form-control" name="dbPort" id="dbPort" min="0" value="<?php echo $_GET['dbPort']; ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="dbUsername">Benutzername</label>
-            <input type="text" class="form-control" name="dbUsername" id="dbUsername" value="<?php echo $_GET['dbUsername']; ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="dbPassword">Passwort</label>
-            <input type="password" class="form-control" name="dbPassword" id="dbPassword" required>
-            <small id="dbPassword" class="form-text text-muted">Hinweis: Das Passwort wird in Klartext in der Datenbank gespeichert! Verwenden Sie einen Datenbank-Benutzer mit eingeschränkten Rechten.</small>
-        </div>
-        <div class="form-group">
-            <label for="dbName">Datenbankname</label>
-            <input type="text" class="form-control" name="dbName" id="dbName" value="<?php echo $_GET['dbName']; ?>" required>
-        </div>
-        <button type="submit" class="btn btn-primary" name="submitAddDb">Hinzufügen</button>
-    </form>
+    </div>
 
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>

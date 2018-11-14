7dda9c42d22921a901dfca876a3ba83042713d4b
albrecht-net <jonasalbrecht9@gmail.com>
2018-11-14 21:30:51 +0100

    JavaScript für Input aktivierung bei Vorlage

--- buchung.php
+++ buchung.php
@@ -13,12 +13,12 @@ if (!$lsc) {
 require_once 'includes/userDbConnect.inc.php';
 
 // Überprüfen ob Submit geklickt wurde
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit']) && ($_POST['addAsTemplate'] == 0)) {
     if (!include 'includes/saveBuchung.inc.php') {
         echo date('H:i:s') . ' Datei einbinden fehlgeschlagen';
         exit();
     }
-} elseif (isset($_POST['submitTemplate'])) {
+} elseif (isset($_POST['submit']) && ($_POST['addAsTemplate'] == 1)) {
     if (!include 'includes/addBuchungTemplate.inc.php') {
         echo date('H:i:s') . ' Datei einbinden fehlgeschlagen';
         exit();
@@ -183,10 +183,10 @@ if (isset($_POST['submit'])) {
         
                             // Prüfen ob Datensätze vorhanden
                             if (mysqli_num_rows($result) < 1): ?>
-                            <select class="form-control" id="kontoSoll" name="kontoSoll">
+                            <select class="form-control chk-toggle-req-slave" id="kontoSoll" name="kontoSoll" required>
                                 <option disabled>Keine Datensätze vorhanden</option>
                             <?php else: ?>
-                            <select class="form-control" id="kontoSoll" name="kontoSoll" required>
+                            <select class="form-control chk-toggle-req-slave" id="kontoSoll" name="kontoSoll" required>
                                 <option></option>
                                 <?php while ($row = mysqli_fetch_assoc($result)): ?>
                                 <option value="<?php echo $row['kontoID']; ?>"<?php echo ($_GET['kontoSoll'] == $row['kontoID'] ? ' selected' : ''); ?>><?php echo str_pad($row['kontoID'], 5, ' ') . $row['bezeichnung']; ?></option>
@@ -203,10 +203,10 @@ if (isset($_POST['submit'])) {
 
                             // Prüfen ob Datensätze vorhanden
                             if (mysqli_num_rows($result) < 1): ?>
-                            <select class="form-control" id="kontoHaben" name="kontoHaben">
+                            <select class="form-control chk-toggle-req-slave" id="kontoHaben" name="kontoHaben" required>
                                 <option disabled>Keine Datensätze vorhanden</option>
                             <?php else: ?>
-                            <select class="form-control" id="kontoHaben" name="kontoHaben" required>
+                            <select class="form-control chk-toggle-req-slave" id="kontoHaben" name="kontoHaben" required>
                                 <option></option>
                                 <?php while ($row = mysqli_fetch_assoc($result)): ?>
                                 <option value="<?php echo $row['kontoID']; ?>"<?php echo ($_GET['kontoHaben'] == $row['kontoID'] ? ' selected' : ''); ?>><?php echo str_pad($row['kontoID'], 5, ' ') . $row['bezeichnung']; ?></option>
@@ -216,7 +216,7 @@ if (isset($_POST['submit'])) {
                         </div>
                         <div class="form-group col-md-2"> <!-- Betrag -->
                             <label for="totalbetrag">Betrag</label>
-                            <input class="form-control" type="number" id="totalbetrag" name="totalbetrag" step="0.01" lang="en" value="<?php echo $_GET['totalbetrag']; ?>" required>
+                            <input class="form-control chk-toggle-req-slave" type="number" id="totalbetrag" name="totalbetrag" step="0.01" lang="en" value="<?php echo $_GET['totalbetrag']; ?>" required>
                         </div>
                     </div>
                     <div class="row">
@@ -326,28 +326,27 @@ if (isset($_POST['submit'])) {
                     <dt class="col-sm-3">Lesezeichen</dt>
                     <dd class="col-sm-9">Beim Speichern als Lesezeichen wird eine URL erzeugt, welche als Lesezeichen verwenden werden kann. Die Vorlage wird nicht zusätzlich gespeichert.</dd>
                 </dl>
+                <div class="form-group form-check"> <!-- Als Vorlage -->
+                    <input class="form-check-input chk-toggle-master" type="checkbox" id="addAsTemplate" name="addAsTemplate" value="1">
+                    <label class="form-check-label" for="addAsTemplate">Als Vorlage hinzufügen</label>
+                </div>
                 <div class="row">
                     <div class="form-group col-12"> <!-- Beschreibung -->
                         <label for="nameTemplate">Beschreibung</label>
-                        <input class="form-control" type="text" id="nameTemplate" name="nameTemplate">
+                        <input class="form-control chk-toggle-dis-slave" type="text" id="nameTemplate" name="nameTemplate" required disabled>
                     </div>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radioTemplate" id="radioTemplate1" value="1" checked>
+                    <input class="form-check-input chk-toggle-dis-slave" type="radio" name="radioTemplate" id="radioTemplate1" value="1" checked disabled>
                     <label class="form-check-label" for="radioTemplate1">
                         In der Applikation
                     </label>
                 </div>
                 <div class="form-group form-check">
-                    <input class="form-check-input" type="radio" name="radioTemplate" id="radioTemplate2" value="2">
+                    <input class="form-check-input chk-toggle-dis-slave" type="radio" name="radioTemplate" id="radioTemplate2" value="2" disabled>
                     <label class="form-check-label" for="radioTemplate2">
                         Als Lesezeichen
                     </label>
-                </div>
-                <div class="row">
-                    <div class="col-6 col-md-3">
-                        <button type="submitTemplate" class="btn btn-secondary btn-block" name="submitTemplate" formnovalidate="">Favorit speichern</button>
-                    </div>
                 </div>
                 </form>
             </div>
@@ -363,5 +362,17 @@ if (isset($_POST['submit'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- Checkbox Toggle -->
+    <script>
+        $(".chk-toggle-master").on('click', function() {
+            if($(this).prop('checked')) {
+                $(".chk-toggle-dis-slave").prop('disabled', false);
+                $(".chk-toggle-req-slave").prop('required', false);
+            } else {
+                $(".chk-toggle-dis-slave").prop('disabled', true);
+                $(".chk-toggle-req-slave").prop('required', true);
+            }
+        })
+    </script>
 </body>
 </html>

c3bf810fb71e2bf5fedc4544a44db5fec293012b
albrecht-net <jonasalbrecht9@gmail.com>
2018-12-17 20:29:24 +0100

    Vorhandene Daueraufträge auflisten

--- standingOrder.php
+++ standingOrder.php
@@ -30,6 +30,9 @@ if (isset($_POST['submit'])) {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/mainSite.css">
+
     <title>Dauerauftrag</title>
 </head>
 <body>
@@ -93,7 +96,7 @@ if (isset($_POST['submit'])) {
                             <label for="template">Buchungsvorlage auswählen</label>
                             <?php
                             // SQL-Query bereitstellen
-                            $sqlquery = "SELECT `templateID`, `name` FROM `template` ORDER BY `name` ASC";
+                            $sqlquery = "SELECT `templateID`, `label` FROM `template` ORDER BY `label` ASC";
                             $result = mysqli_query($userLink, $sqlquery);
 
                             // Prüfen ob Datensätze vorhanden
@@ -104,7 +107,7 @@ if (isset($_POST['submit'])) {
                             <select class="form-control" id="template" name="template">
                                 <option></option>
                                 <?php while ($row = mysqli_fetch_assoc($result)): ?>
-                                <option value="<?php echo $row['templateID']; ?>"<?php echo ($_GET['template'] == $row['templateID'] ? ' selected' : ''); ?>><?php echo $row['name']; ?></option>
+                                <option value="<?php echo $row['templateID']; ?>"<?php echo ($_GET['template'] == $row['templateID'] ? ' selected' : ''); ?>><?php echo $row['label']; ?></option>
                                 <?php endwhile;
                             endif; ?>
                             </select>
@@ -214,14 +217,14 @@ if (isset($_POST['submit'])) {
             </div>
         </div>
 
-        <h3 class="mt-3" id="addTemplate">Als Vorlage speichern</h3>
+        <h3 class="mt-3" id="addTemplate">Erfasste Daueraufträge</h3>
         <hr class="mb-4">
         <div class="row">
             <div class="col-12 mb-5">
                 <?php
                 // SQL-Query bereitstellen
-                $sqlquery = "SELECT `templateID`, `created`, `name`, `value` FROM `templates` WHERE `userID` = " . intval($_SESSION['userID']) . " AND `dbID` = " . intval($_SESSION['userDb']['dbID']);
-                $result = mysqli_query($config['link'], $sqlquery);
+                $sqlquery = "SELECT standingOrder.standingOrderID, standingOrder.label AS standingOrderLabel, template.label AS templateLabel, standingOrder.periodicityType, standingOrder.periodicityValue, standingOrder.validToValue, standingOrder.handledEvents, standingOrder.remainingEvents, standingOrder.nextExecutionDate FROM standingOrder LEFT JOIN template ON standingOrder.template = template.templateID";
+                $result = mysqli_query($userLink, $sqlquery);
 
                 // Prüfen ob Datensätze vorhanden
                 if (mysqli_num_rows($result) >= 1):
@@ -233,27 +236,49 @@ if (isset($_POST['submit'])) {
                                 <th scope="col">Erstelldatum</th>
                                 <th scope="col">Name</th>
                                 <th scope="col">Vorlage</th>
+                                <th scope="col">Wiederholung</th>
+                                <th scope="col">Nächste Ausführung</th>
+                                <th scope="col">Anzahl abgearbeitet</th>
+                                <th scope="col">Anzahl verbleibend</th>
+                                <th scope="col">Enddatum</th>
                                 <th scope="col"></th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php while ($row = mysqli_fetch_assoc($result)):
-                                
-                            // Decode json value
-                            $valueDecoded = json_decode($row['value'], TRUE); ?>
+                                switch ($row['periodicityType']) {
+                                    case '1':
+                                        $row['periodicityType'] = 'Tage';
+                                        break;
+                                    case '2':
+                                        $row['periodicityType'] = 'Wochen';
+                                        break;
+                                    case '4':
+                                        $row['periodicityType'] = 'Monate';
+                                        break;
+                                    case '8':
+                                        $row['periodicityType'] = 'Jahre';
+                                        break;
+                                }
+                            ?>
                             <tr>
                                 <td><?php echo date_format(date_create($row['created']), 'd.m.Y'); ?></td>
-                                <td><a href="buchung.php?<?php echo http_build_query($valueDecoded); ?>"><?php echo $row['name']; ?></a></td>
-                                <td><?php echo implode(', ', array_keys($valueDecoded)); ?></td>
-                                <td><button type="button" class="btn btn-tr btn-block btn-danger tr-delete" value="templates-<?php echo $row['templateID']; ?>">Löschen</button></td>
+                                <td><?php echo $row['standingOrderLabel']; ?></td>
+                                <td><?php echo $row['templateLabel']; ?></td>
+                                <td><?php echo 'Alle ' . $row['periodicityValue'] . ' ' . $row['periodicityType']; ?></td>
+                                <td><?php echo date_format(date_create($row['nextExecutionDate']), 'd.m.Y'); ?></td>
+                                <td><?php echo ($row['handledEvents'] == NULL ? 0 : $row['handledEvents']); ?></td>
+                                <td><?php echo ($row['remainingEvents'] == NULL ? '-' : $row['remainingEvents']); ?></td>
+                                <td><?php echo ($row['validToValue'] == NULL ? '-' : date_format(date_create($row['validToValue']), 'd.m.Y')); ?></td>
+                                <td><button type="button" class="btn btn-tr btn-block btn-danger tr-delete" value="templates-<?php echo $row['standingorderID']; ?>">Löschen</button></td>
                             </tr>
                             <?php endwhile; ?>
                         </tbody>
                     </table>
                 </div>
                 <?php else: ?>
-                <p class="lead">Keine Vorlage gefunden</p>
-                <p>Sie haben für die ausgewählte Ziel-Datenbank noch keine Vorlage erstellt. Erstellen Sie Ihre erste Vorlage gleich <a href="buchung.php#addTemplate">hier</a>.</p>
+                <p class="lead">Keine Einträge gefunden</p>
+                <p>Sie haben für die ausgewählte Ziel-Datenbank noch keinen Dauerauftrag erstellt. Erstellen Sie Ihren ersten Dauerauftrag gleich <a href="standingOrder.php#addStandingOrder">hier</a>.</p>
                 <?php endif; ?>
                 </div>
             </div>

5758137a8b8d07fb3791c719b3af0acb020f22c5
albrecht-net <jonasalbrecht9@gmail.com>
2018-12-17 18:49:51 +0100

    Anzahl fälliger Daueraufträge ermitteln überarbeitet

--- includes/standingOrderCheck.inc.php
+++ includes/standingOrderCheck.inc.php
@@ -1,10 +1,27 @@
 <?php
-$date = date('Y-m-d H');
-
-// Anzahl fälliger Daueraufträge ermitteln und in Session zwischenspeichern
-if ($_SESSION['standingOrder']['updated'] < $date) {
+// Anzahl fälliger Daueraufträge ermitteln und in Cookie zwischenspeichern
+if (!isset($_COOKIE['standingOrder']) || (json_decode($_COOKIE['standingOrder'], TRUE)['userID'] != intval($_SESSION['userID'])) || (json_decode($_COOKIE['standingOrder'], TRUE)['dbID'] != intval($_SESSION['userDb']['dbID']))) {
     $sqlquery = "SELECT `standingOrderID` FROM `standingOrder` WHERE `nextExecutionDate` <= NOW()";
-    $_SESSION['standingOrder']['count'] = mysqli_num_rows(mysqli_query($userLink, $sqlquery));
-    $_SESSION['standingOrder']['updated'] = $date;
+
+    $values = array(
+        'userID' => intval($_SESSION['userID']),
+        'dbID' => intval($_SESSION['userDb']['dbID']),
+        'count' => mysqli_num_rows(mysqli_query($userLink, $sqlquery))
+    );
+
+    $datePlus = strtotime('+8 hour');
+    $dateTomorrow = strtotime('tomorrow');
+
+    $json = json_encode($values, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
+
+    // Setzt das Abblaufdatum des Cokkie auf +8h, ausser es überschreitet 00:00 Uhr vom nächsten Tag
+    if ($datePlus < $dateTomorrow) {
+        setcookie('standingOrder', $json, $datePlus, '/');
+    } else {
+        setcookie('standingOrder', $json, $dateTomorrow, '/');
+    }
+
+    // Wert für Direktzugriff bereitstellen
+    $_COOKIE['standingOrder'] = $json;
 }
 ?>

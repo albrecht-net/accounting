a13815a665fabb5349459f3f3ed53330ea606dbd
albrecht-net <jonasalbrecht9@gmail.com>
2018-12-30 20:05:29 +0100

    Überprüfung Dauerauftrag Enddatum > nächstes Ausführdatum

--- includes/addStandingOrder.inc.php
+++ includes/addStandingOrder.inc.php
@@ -23,52 +23,60 @@ if (count($dataInput) > 0) {
         'updated' => 'NOW()'
     );
 
-    // Erstes (nächstes) Ausführdatum festlegen
-    if ($dataInput['validFromType'] == 1) { // Nutze Startdatum
-        $dataInput['nextExecutionDate'] = $dataInput['validFromValue'];
-    } elseif ($dataInput['validFromType'] == 2) { // Nutze Monatsende
-        $dataInput['nextExecutionDate'] = date_format(date_create($dataInput['validFromValue']), 'Y-m-t');
-    } else {
-        $msg['inputError'] = 1;
-    }
-
-    // Enddatum festlegen
-    if ($dataInput['validToType'] == 1) { // Kein Enddatum
-        unset($dataInput['validToValue']);
-    } elseif ($dataInput['validToType'] == 2) { // Nutze Enddatum
-    } elseif ($dataInput['validToType'] == 4) { // Anzahl Wiederholungen
-        switch ($dataInput['periodicityType']) {
-            case 1: // Tag
-                $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' day'), 'Y-m-d');
-                break;
-            case 2: // Woche
-                $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' week'), 'Y-m-d');
-                break;
-            case 4: // Monat
-                $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' month'), 'Y-m-d');
-                break;
-            case 8: // Jahr
-                $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' year'), 'Y-m-d');
-                break;
-            default:
-                $msg['inputError'];
+    do {
+        // Erstes (nächstes) Ausführdatum festlegen
+        if ($dataInput['validFromType'] == 1) { // Nutze Startdatum
+            $dataInput['nextExecutionDate'] = $dataInput['validFromValue'];
+        } elseif ($dataInput['validFromType'] == 2) { // Nutze Monatsende
+            $dataInput['nextExecutionDate'] = date_format(date_create($dataInput['validFromValue']), 'Y-m-t');
+        } else {
+            $msg['inputError'] = 1;
+            break;
         }
-    } else {
-        $msg['inputError'] = 1;
-    }
 
-    // SQL-Query bereitstellen
-    $columns = "`" . implode("`, `", array_keys($dataInput)) . "`, `" . implode("`, `", array_keys($dataFunctions)) . "`";
-    $values = "'" . implode("', '", $dataInput) . "', " . implode(", ", $dataFunctions);
-    $sqlquery = "INSERT INTO `standingOrder` (" . $columns . ") VALUES (" . $values . ")";
+        // Enddatum festlegen
+        if ($dataInput['validToType'] == 1) { // Kein Enddatum
+            unset($dataInput['validToValue']);
+        } elseif ($dataInput['validToType'] == 2) { // Nutze Enddatum
+            if ($dataInput['validToValue'] <= $dataInput['nextExecutionDate']) {
+                break;
+            }
+        } elseif ($dataInput['validToType'] == 4) { // Anzahl Wiederholungen
+            switch ($dataInput['periodicityType']) {
+                case 1: // Tag
+                    $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' day'), 'Y-m-d');
+                    break;
+                case 2: // Woche
+                    $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' week'), 'Y-m-d');
+                    break;
+                case 4: // Monat
+                    $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' month'), 'Y-m-d');
+                    break;
+                case 8: // Jahr
+                    $dataInput['validToValue'] = date_format(date_modify(date_create($dataInput['nextExecutionDate']), $dataInput['periodicityValue'] * ($dataInput['initialEvents'] - 1) . ' year'), 'Y-m-d');
+                    break;
+                default:
+                    $msg['inputError'];
+                    break 2;
+            }
+        } else {
+            $msg['inputError'] = 1;
+            break;
+        }
 
-    // SQL-Query ausführen und überprüfen
-    if (!mysqli_query($userLink, $sqlquery)) {
-        $msg['sqlInsertError'] = 1;
+        // SQL-Query bereitstellen
+        $columns = "`" . implode("`, `", array_keys($dataInput)) . "`, `" . implode("`, `", array_keys($dataFunctions)) . "`";
+        $values = "'" . implode("', '", $dataInput) . "', " . implode(", ", $dataFunctions);
+        $sqlquery = "INSERT INTO `standingOrder` (" . $columns . ") VALUES (" . $values . ")";
 
-    } else {
-        $msg['success'] = 1;
-    }
+        // SQL-Query ausführen und überprüfen
+        if (!mysqli_query($userLink, $sqlquery)) {
+            $msg['sqlInsertError'] = 1;
+
+        } else {
+            $msg['success'] = 1;
+        }
+    } while (0);
 } else {
     $msg['noInput'] = 1;
 }

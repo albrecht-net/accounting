8c0c2f7646d54569c99106366c91bac475015afd
albrecht-net <jonasalbrecht9@gmail.com>
2019-02-03 17:57:40 +0100

    login.inc abgeändert für Direktaufruf

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,73 +1,84 @@
 <?php
-// Array Eingabe
-$dataInput = array(
-    'username' => mysqli_real_escape_string($config['link'], $_POST['inputUsername']),
-    'password' => $_POST['inputPassword']
-);
+session_start();
 
-// SQL-Query bereitstellen
-$sqlquery = "SELECT `username`, `password`, `userID` FROM `users` WHERE `username` = '" . $dataInput['username'] . "' AND `activation` = 'Y' AND `status` = 'Y'";
-$result = mysqli_query($config['link'], $sqlquery);
+if (isset($_POST['submit'])) {
 
-// Benutzer abfragen
-if (mysqli_num_rows($result) != 1) {
-    $msg['invalid'] = 1;
-} else {
-    // Abfrage in Array schreiben
-    $dataDb = mysqli_fetch_assoc($result);
+    // Konfiguration einbinden
+    require_once '../config.php';
 
-    // Passwort validieren
-    if (!password_verify($dataInput['password'], $dataDb['password'])) {
+    // Array Eingabe
+    $dataInput = array(
+        'username' => mysqli_real_escape_string($config['link'], $_POST['inputUsername']),
+        'password' => $_POST['inputPassword']
+    );
+
+    // SQL-Query bereitstellen
+    $sqlquery = "SELECT `username`, `password`, `userID` FROM `users` WHERE `username` = '" . $dataInput['username'] . "' AND `activation` = 'Y' AND `status` = 'Y'";
+    $result = mysqli_query($config['link'], $sqlquery);
+
+    // Benutzer abfragen
+    if (mysqli_num_rows($result) != 1) {
         $msg['invalid'] = 1;
     } else {
-        // Benutzerdaten in Session schreiben
-        $_SESSION['userID'] = intval($dataDb['userID']);
-        $_SESSION['username'] = $dataDb['username'];
+        // Abfrage in Array schreiben
+        $dataDb = mysqli_fetch_assoc($result);
 
-        // Überprüfen ob Benutzer eine Standarddatenbank hat
-        if (!$dataInputGet['forceDatabaseSelect']) {
-            // SQL-Query bereitstellen
-            $sqlquery = "SELECT `defaultDb` FROM `userconfig` WHERE `userID` = " . $dataDb['userID'];
-            $result = mysqli_query($config['link'], $sqlquery);
+        // Passwort validieren
+        if (!password_verify($dataInput['password'], $dataDb['password'])) {
+            $msg['invalid'] = 1;
+        } else {
+            // Benutzerdaten in Session schreiben
+            $_SESSION['userID'] = intval($dataDb['userID']);
+            $_SESSION['username'] = $dataDb['username'];
 
-            if (mysqli_num_rows($result) == 1) {
-                // Abfrage in Array schreiben
-                $dataDb = mysqli_fetch_assoc($result);
+            // Überprüfen ob Benutzer eine Standarddatenbank hat
+            if (!$dataInputGet['forceDatabaseSelect']) {
+                // SQL-Query bereitstellen
+                $sqlquery = "SELECT `defaultDb` FROM `userconfig` WHERE `userID` = " . $dataDb['userID'];
+                $result = mysqli_query($config['link'], $sqlquery);
 
-                if (!empty($dataDb['defaultDb'])) {
-                    // Datenbank ID in Session schreiben
-                    $_SESSION['userDb']['dbID'] = intval($dataDb['defaultDb']);
+                if (mysqli_num_rows($result) == 1) {
+                    // Abfrage in Array schreiben
+                    $dataDb = mysqli_fetch_assoc($result);
 
-                    $_SESSION['userDb']['userDbSet'] = 1;
+                    if (!empty($dataDb['defaultDb'])) {
+                        // Datenbank ID in Session schreiben
+                        $_SESSION['userDb']['dbID'] = intval($dataDb['defaultDb']);
 
-                    // Mit Ziel Datenbank verbinden
-                    if (include_once 'userDbConnect.inc.php') {
-                        // Fällige Daueraufträge prüfen
-                        include 'standingOrderCheck.inc.php';
+                        $_SESSION['userDb']['userDbSet'] = 1;
+
+                        // Mit Ziel Datenbank verbinden
+                        if (include_once 'userDbConnect.inc.php') {
+                            // Fällige Daueraufträge prüfen
+                            include 'standingOrderCheck.inc.php';
+                        }
+
+                        // Weiterleitung
+                        if (empty($dataInputGet['rd'])) {
+                            header('Location: index.php');
+                        } else {
+                            $rd = $dataInputGet['rd'];
+                            header('Location: ' . $rd);
+                        }
+                        exit();
                     }
-
-                    // Weiterleitung
-                    if (empty($dataInputGet['rd'])) {
-                        header('Location: index.php');
-                    } else {
-                        $rd = $dataInputGet['rd'];
-                        header('Location: ' . $rd);
-                    }
-                    exit();
                 }
             }
-        }
-        // Benutzerdatenbank
-        $_SESSION['userDb']['userDbSet'] = 0;
+            // Benutzerdatenbank
+            $_SESSION['userDb']['userDbSet'] = 0;
 
-        // Weiterleitung
-        if (empty($dataInputGet['rd'])) {
-            header('Location: selectDatabase.php');
-        } else {
-            $rd = $dataInputGet['rd'];
-            header('Location: selectDatabase.php?rd=' . urlencode($rd));
+            // Weiterleitung
+            if (empty($dataInputGet['rd'])) {
+                header('Location: selectDatabase.php');
+            } else {
+                $rd = $dataInputGet['rd'];
+                header('Location: selectDatabase.php?rd=' . urlencode($rd));
+            }
+            exit();
         }
-        exit();
     }
+} else {
+    http_response_code(405);
+    header ('Allow: POST');
 }
 ?>

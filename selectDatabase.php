d339751fe5bd4927f655ec40518ca3b3be7f4d5f
albrecht-net <jonasalbrecht9@gmail.com>
2019-02-06 18:26:00 +0100

    selectDatabase-Form action angepasst

--- selectDatabase.php
+++ selectDatabase.php
@@ -10,17 +10,6 @@ if (!$lsc) {
     header('Location: login.php');
     exit();
 }
-
-// Überprüfen ob Submit geklickt wurde
-if (isset($_POST['submit']) && !empty($_POST['dbID'])) {
-    if (!include 'includes/selectDatabase.inc.php') {
-        echo date('H:i:s') . ' Datei einbinden fehlgeschlagen';
-        exit();
-    }
-}
-
-// Fällige Daueraufträge prüfen
-include 'includes/standingOrderCheck.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -41,55 +30,56 @@ include 'includes/standingOrderCheck.inc.php';
     <div class="form-group-database">
         <h3>Datenbank auswählen</h3>
         <?php if (!$_SESSION['userDb']['userDbSet']): // Überprüfen ob Benutzer Db ausgewählt wurde ?>
-        <p>Bitte wählen Sie die Zieldatenbank aus, auf welcher die Eingaben der Aktuellen Sitzung geschrieben werden sollen.</p>
-        
-        <?php
-        // Prüfen ob eine Datenbank für den angemeldeten Benutzer verfügbar ist
-        $sqlquery = "SELECT `dbID`, `dbHost`, `dbName` FROM `databases` WHERE `userID` = '" . intval($_SESSION['userID']) . "'";
-        $result = mysqli_query($config['link'], $sqlquery);
-        if (mysqli_num_rows($result) >= 1):
+            <?php include_once 'includes/alertProvider.inc.php'; // Alert Provider ?>
+            <p>Bitte wählen Sie die Zieldatenbank aus, auf welcher die Eingaben der Aktuellen Sitzung geschrieben werden sollen.</p>
 
-            if (empty($_GET['rd'])): ?>
-            <form action="selectDatabase.php" method="POST">
+            <?php
+            // Prüfen ob eine Datenbank für den angemeldeten Benutzer verfügbar ist
+            $sqlquery = "SELECT `dbID`, `dbHost`, `dbName` FROM `databases` WHERE `userID` = '" . intval($_SESSION['userID']) . "'";
+            $result = mysqli_query($config['link'], $sqlquery);
+            if (mysqli_num_rows($result) >= 1):
+
+                if (empty($_GET['rd'])): ?>
+                <form action="includes/selectDatabase.inc.php" method="POST">
+                <?php else: ?>
+                <form action="includes/selectDatabase.inc.php?rd=<?php echo urlencode($_GET['rd']); ?>" method="POST">
+                <?php endif; ?>
+                    <div class="form-group"> <!-- Datenbank Select -->
+                        <select class="form-control" id="dbID" name="dbID">
+                            <option disabled selected>Datenbank auswählen</option>
+                            <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                            <option value="<?php echo $row['dbID']; ?>"><?php echo $row['dbName'] . ', ' . $row['dbHost']; ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
+                    <div class="form-group"> <!-- Datenbank speichern -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="saveDbSelection" name="saveDbSelection" value="1">
+                            <label class="form-check-label" for="saveDbSelection">Auswahl speichern</label>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <button type="submit" class="btn btn-primary btn-block" name="submit">Bestätigen</button>
+                        </div>
+                        <div class="col">
+                            <?php if (empty($_GET['rd'])): ?>
+                            <a href="index.php" class="btn btn-secondary btn-block" role="button">Überspringen</a>
+                            <?php else: ?>
+                            <a href="<?php echo $_GET['rd']; ?>" class="btn btn-secondary btn-block" role="button">Überspringen</a>
+                            <?php endif ?>
+                        </div>
+                    </div>
+                </form>
+
             <?php else: ?>
-            <form action="selectDatabase.php?rd=<?php echo urlencode($_GET['rd']); ?>" method="POST">
+                <p>Es wurde keine Datenbank welche mit Ihrem Account verknüpft ist gefunden!</p>
+                <?php if (empty($_GET['rd'])): ?>
+                <a href="index.php" class="btn btn-primary" role="button">OK</a>
+                <?php else: ?>
+                <a href="<?php echo $_GET['rd']; ?>" class="btn btn-primary" role="button">OK</a>
+                <?php endif ?>
             <?php endif; ?>
-                <div class="form-group"> <!-- Datenbank Select -->
-                    <select class="form-control" id="dbID" name="dbID">
-                        <option disabled selected>Datenbank auswählen</option>
-                        <?php while ($row = mysqli_fetch_assoc($result)): ?>
-                        <option value="<?php echo $row['dbID']; ?>"><?php echo $row['dbName'] . ', ' . $row['dbHost']; ?></option>
-                        <?php endwhile; ?>
-                    </select>
-                </div>
-                <div class="form-group"> <!-- Datenbank speichern -->
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="saveDbSelection" name="saveDbSelection" value="1">
-                        <label class="form-check-label" for="saveDbSelection">Auswahl speichern</label>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col">
-                        <button type="submit" class="btn btn-primary btn-block" name="submit">Bestätigen</button>
-                    </div>
-                    <div class="col">
-                        <?php if (empty($_GET['rd'])): ?>
-                        <a href="index.php" class="btn btn-secondary btn-block" role="button">Überspringen</a>
-                        <?php else: ?>
-                        <a href="<?php echo $_GET['rd']; ?>" class="btn btn-secondary btn-block" role="button">Überspringen</a>
-                        <?php endif ?>
-                    </div>
-                </div>
-            </form>
-
-        <?php else: ?>
-            <p>Es wurde keine Datenbank welche mit Ihrem Account verknüpft ist gefunden!</p>
-            <?php if (empty($_GET['rd'])): ?>
-            <a href="index.php" class="btn btn-primary" role="button">OK</a>
-            <?php else: ?>
-            <a href="<?php echo $_GET['rd']; ?>" class="btn btn-primary" role="button">OK</a>
-            <?php endif ?>
-        <?php endif; ?>
 
         <?php else: ?>
         <p class="lead">Für die aktuelle Sitzung wurde bereits eine Datenbank ausgewählt.</p>

81c5c684c9b4806a7b9223d8003c4ed9f2468336
albrecht-net <jonasalbrecht9@gmail.com>
2019-03-18 20:51:56 +0100

    Beim Überspringen eines Dauerauftrags wird nur noch ein Sprung gemacht

--- includes/skipStandingOrder.inc.php
+++ includes/skipStandingOrder.inc.php
@@ -37,63 +37,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $dataUpdateFunctions['updated'] = 'NOW()';
 
-        // Loop legt nächstes Ausführdatum fest, welches grösser als das heutige ist. Jeder Sprung erhöht den Event Counter um 1.
-        for ($now = date('Y-m-d', strtotime('now')), $tmpNxtExDate = $dataDb['nextExecutionDate'], $skippedEvents = 0; $now >= $tmpNxtExDate; $skippedEvents++) {
-            // Stop Loop wenn verbleinde Wiederholungen kleiner 0 ist
-            if ($dataDb['validToType'] == 4) { // Anzahl Wiederholungen
-                if ($dataDb['remainingEvents'] - $skippedEvents == 0) {
-                    break;
+        // Nächstes Ausführdatum festlegen
+        switch ($dataDb['periodicityType']) {
+            case 1: // Tag
+                $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create($dataDb['nextExecutionDate']), $dataDb['periodicityValue'] . ' day'), 'Y-m-d');
+                break;
+            case 2: // Woche
+                $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create($dataDb['nextExecutionDate']), $dataDb['periodicityValue'] . ' week'), 'Y-m-d');
+                break;
+            case 4: // Monat
+                if ($dataDb['validFromType'] == 1) {
+                    $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create($dataDb['nextExecutionDate']), $dataDb['periodicityValue'] . ' month'), 'Y-m-d');
+                } elseif ($dataDb['validFromType'] == 2) {
+                    $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create(date_format(date_create($dataDb['nextExecutionDate']), 'Y-m')), $dataDb['periodicityValue'] . ' month'), 'Y-m-t');
                 }
-            }
-
-            // Nächstes Ausführdatum festlegen
-            switch ($dataDb['periodicityType']) {
-                case 1: // Tag
-                    $tmpNxtExDate = date_format(date_modify(date_create($tmpNxtExDate), $dataDb['periodicityValue'] . ' day'), 'Y-m-d');
-                    break;
-                case 2: // Woche
-                    $tmpNxtExDate = date_format(date_modify(date_create($tmpNxtExDate), $dataDb['periodicityValue'] . ' week'), 'Y-m-d');
-                    break;
-                case 4: // Monat
-                    if ($dataDb['validFromType'] == 1) {
-                        $tmpNxtExDate = date_format(date_modify(date_create($tmpNxtExDate), $dataDb['periodicityValue'] . ' month'), 'Y-m-d');
-                    } elseif ($dataDb['validFromType'] == 2) {
-                        $tmpNxtExDate = date_format(date_modify(date_create(date_format(date_create($tmpNxtExDate), 'Y-m')), $dataDb['periodicityValue'] . ' month'), 'Y-m-t');
-                    }
-                    break;
-                case 8: // Jahr
-                    if ($dataDb['validFromType'] == 1) {
-                        $tmpNxtExDate = date_format(date_modify(date_create($tmpNxtExDate), $dataDb['periodicityValue'] . ' year'), 'Y-m-d');
-                    } elseif ($dataDb['validFromType'] == 2) {
-                        $tmpNxtExDate = date_format(date_modify(date_create(date_format(date_create($tmpNxtExDate), 'Y-m')), $dataDb['periodicityValue'] . ' year'), 'Y-m-t');
-                    }
-                    break;
-                case 16: // Montag - Freitag
-                    $tmpNxtExDate = date_format(date_modify(date_create($tmpNxtExDate), $dataDb['periodicityValue'] . ' weekday'), 'Y-m-d');
-                    break;
-            }
+                break;
+            case 8: // Jahr
+                if ($dataDb['validFromType'] == 1) {
+                    $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create($dataDb['nextExecutionDate']), $dataDb['periodicityValue'] . ' year'), 'Y-m-d');
+                } elseif ($dataDb['validFromType'] == 2) {
+                    $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create(date_format(date_create($dataDb['nextExecutionDate']), 'Y-m')), $dataDb['periodicityValue'] . ' year'), 'Y-m-t');
+                }
+                break;
+            case 16: // Montag - Freitag
+                $dataUpdate['nextExecutionDate'] = date_format(date_modify(date_create($dataDb['nextExecutionDate']), $dataDb['periodicityValue'] . ' weekday'), 'Y-m-d');
+                break;
         }
-
-        // Nächstes Ausführdatum aus Loop an dataUpdate zuweisen
-        $dataUpdate['nextExecutionDate'] = $tmpNxtExDate;
 
         // Auf Abschluss prüfen, wenn gültig bis Widerruf dann keine Überprüfung
         if ($dataDb['validToType'] == 1) { // Kein Enddatum
             // Event Counter
-            $dataUpdateFunctions['handledEvents'] = 'handledEvents +' . $skippedEvents;
+            $dataUpdateFunctions['handledEvents'] = 'handledEvents +1';
         } elseif ($dataDb['validToType'] == 2) { // Enddatum
             // Event Counter
-            $dataUpdateFunctions['handledEvents'] = 'handledEvents +' . $skippedEvents;
+            $dataUpdateFunctions['handledEvents'] = 'handledEvents +1';
 
-            if ($dataDb['validToValue'] < $dataDb['nextExecutionDate']) {
+            if ($dataDb['validToValue'] < $dataUpdate['nextExecutionDate']) {
                 unset($dataUpdate['nextExecutionDate']);
                 $dataUpdateFunctions['nextExecutionDate'] = 'NULL';
                 $dataUpdate['closed'] = 'Y';
             }
         } elseif ($dataDb['validToType'] == 4) { // Anzahl Wiederholungen
             // Event Counter
-            $dataUpdateFunctions['handledEvents'] = 'handledEvents +' . $skippedEvents;
-            $dataUpdate['remainingEvents'] = $dataDb['remainingEvents'] - $skippedEvents;
+            $dataUpdateFunctions['handledEvents'] = 'handledEvents +1';
+            $dataUpdate['remainingEvents'] = $dataDb['remainingEvents'] - 1;
 
             if ($dataUpdate['remainingEvents'] == 0) {
                 unset($dataUpdate['nextExecutionDate']);

ff85f4f26e98116b5da0508d525b20679e3eb8be
albrecht-net <jonasalbrecht9@gmail.com>
2019-05-08 20:10:40 +0200

    Filter-Eingabefelder erstellt

--- index.php
+++ index.php
@@ -44,6 +44,123 @@ include 'includes/standingOrderCheck.inc.php';
                     <h3 class="mt-3" id="lastEntries">Zuletzt erfasste Buchungen</h3>
                     <hr class="mb-4">
                     <div class="row">
+                        <div class="col-12 mb-4">
+                            <div class="form-row">
+                                <div class="form-group col-md-5"> <!-- Konto Soll -->
+                                    <div class="form-row">
+                                        <label class="mr-4" for="debitAccount">Konto Soll</label>
+                                        <div class="form-group form-check mb-2">
+                                            <input class="form-check-input" type="checkbox" id="chkFilterDebitAccAll" name="chkFilterDebitAccAll" value="1" checked>
+                                            <label class="form-check-label" for="chkFilterDebitAccAll">Alle auswählen</label>
+                                        </div>
+                                    </div>
+                                    <?php
+                                    // SQL-Query bereitstellen
+                                    $sqlquery = "SELECT `account`.`accountID`, `account`.`label` AS `accountLabel`, `accountCategory`.`label` AS `categoryLabel` FROM `account` LEFT JOIN `accountCategory` ON `account`.`category` = `accountCategory`.`categoryID` WHERE `account`.`active` = 'Y' ORDER BY `accountCategory`.`label` ASC, `account`.`label` ASC";
+                                    $result = mysqli_query($userLink, $sqlquery);
+                
+                                    // Prüfen ob Datensätze vorhanden
+                                    if (mysqli_num_rows($result) < 1): ?>
+                                        <select class="form-control chk-toggle-req-slave" id="debitAccount" name="debitAccount" size="6" multiple required>
+                                            <option disabled>Keine Datensätze vorhanden</option>
+                                    <?php else: ?>
+                                        <select class="form-control chk-toggle-req-slave" id="debitAccount" name="debitAccount" size="6" multiple required>
+                                        <?php
+                                        // Resulat in 1 Array schreiben, sortiert nach Kategorie
+                                        $valueArray = [];
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            if ($row['categoryLabel'] != $category) {
+                                                $i = 0;
+                                            }
+        
+                                            $category = $row['categoryLabel'];
+        
+                                            $valueArray[$category][$i] = $row;
+                                            $i++;
+                                        }
+        
+                                        // Array in Dropdown ausgeben
+                                        foreach ($valueArray as $key => $row1): ?>
+                                            <optgroup label="<?php echo htmlspecialchars($key, ENT_QUOTES, 'UTF-8'); ?>">
+                                            <?php foreach ($row1 as $key => $row2): ?>
+                                                <option value="<?php echo intval($row2['accountID']); ?>"<?php echo ($_GET['debitAccount'] == $row2['accountID'] ? ' selected' : ''); ?>><?php echo intval($row2['accountID']) . ' ' . htmlspecialchars($row2['accountLabel'], ENT_QUOTES, 'UTF-8'); ?></option>
+                                            <?php endforeach;
+                                        endforeach;
+                                    endif; ?>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-5"> <!-- Konto Haben -->
+                                    <div class="form-row">
+                                        <label class="mr-4" for="creditAccount">Konto Soll</label>
+                                        <div class="form-group form-check mb-2">
+                                            <input class="form-check-input" type="checkbox" id="chkFilterCreditAccAll" name="chkFilterCreditAccAll" value="1" checked>
+                                            <label class="form-check-label" for="chkFilterCreditAccAll">Alle auswählen</label>
+                                        </div>
+                                    </div>
+                                    <?php
+                                    // SQL-Query bereitstellen
+                                    $sqlquery = "SELECT `account`.`accountID`, `account`.`label` AS `accountLabel`, `accountCategory`.`label` AS `categoryLabel` FROM `account` LEFT JOIN `accountCategory` ON `account`.`category` = `accountCategory`.`categoryID` WHERE `account`.`active` = 'Y' ORDER BY `accountCategory`.`label` ASC, `account`.`label` ASC";
+                                    $result = mysqli_query($userLink, $sqlquery);
+        
+                                    // Prüfen ob Datensätze vorhanden
+                                    if (mysqli_num_rows($result) < 1): ?>
+                                    <select class="form-control chk-toggle-req-slave" id="creditAccount" name="creditAccount" size="6" multiple required>
+                                        <option disabled>Keine Datensätze vorhanden</option>
+                                    <?php else: ?>
+                                    <select class="form-control chk-toggle-req-slave" id="creditAccount" name="creditAccount" size="6" multiple required>
+                                        <?php
+                                        // Resulat in 1 Array schreiben, sortiert nach Kategorie
+                                        $valueArray = [];
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            if ($row['categoryLabel'] != $category) {
+                                                $i = 0;
+                                            }
+
+                                            $category = $row['categoryLabel'];
+
+                                            $valueArray[$category][$i] = $row;
+                                            $i++;
+                                        }
+
+                                        // Array in Dropdown ausgeben
+                                        foreach ($valueArray as $key => $row1): ?>
+                                            <optgroup label="<?php echo htmlspecialchars($key, ENT_QUOTES, 'UTF-8'); ?>">
+                                            <?php foreach ($row1 as $key => $row2): ?>
+                                                <option value="<?php echo intval($row2['accountID']); ?>"<?php echo ($_GET['creditAccount'] == $row2['accountID'] ? ' selected' : ''); ?>><?php echo intval($row2['accountID']) . ' ' . htmlspecialchars($row2['accountLabel'], ENT_QUOTES, 'UTF-8'); ?></option>
+                                            <?php endforeach;
+                                        endforeach;
+                                    endif; ?>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-2"> <!-- Zeitraum -->
+                                    <label for="aeSelPeriodOfAE">Auswahl Zeitraum</label>
+                                    <select class="form-control" id="aeSelPeriodOfAE" name="aeSelPeriodOfAE" required>
+                                        <option value="1">Laufender Monat</option>
+                                        <option value="2">Laufendes Quartal</option>
+                                        <option value="4">Laufendes Jahr</option>
+                                        <option value="8">Letzter Monat</option>
+                                        <option disabled></option>
+                                        <option value="16">Letzte 30 Tage</option>
+                                        <option value="32">Letzte 90 Tage</option>
+                                        <option value="64">Letzte 180 Tage</option>
+                                        <option value="128">Letzte 360 Tage</option>
+                                        <option disabled></option>
+                                        <option value="256" selected>Letzte 10 Buchungen</option>
+                                        <option value="512">Letzte 20 Buchungen</option>
+                                        <option value="1024">Letzte 30 Buchungen</option>
+                                        <option value="2048">Letzte 100 Buchungen</option>
+                                        <option value="4096">Letzte 1000 Buchungen</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 col-md-3">
+                                    <button type="submit" class="btn btn-primary btn-block" name="submit">Auswahl anzeigen</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-12 mb-5">
                             <?php
                             // SQL-Query bereitstellen
